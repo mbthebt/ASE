b66b12205ea1bf09f74a0ad6c093004c4b23ae62
Hannes Heuberger <hannes.up@gmail.com>
2019-01-02 15:50:09 +0100

    added tab bar navigation

--- mobile/src/main/java/com/example/admin/emotiontracker/MainActivity.kt
+++ mobile/src/main/java/com/example/admin/emotiontracker/MainActivity.kt
@@ -1,29 +1,48 @@
 package com.example.admin.emotiontracker
 
+import android.app.Fragment
+import android.net.Uri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    FeelingsFragment.OnFragmentInteractionListener,
+    ActivityFragment.OnFragmentInteractionListener,
+    DashboardFragment.OnFragmentInteractionListener {
+
+    override fun onFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
                 message.setText(R.string.title_home)
+                replaceFragment(ActivityFragment.newInstance("", ""))
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
                 message.setText(R.string.title_dashboard)
+                replaceFragment(DashboardFragment.newInstance("", ""))
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
                 message.setText(R.string.title_notifications)
+                replaceFragment(FeelingsFragment.newInstance("", ""))
                 return@OnNavigationItemSelectedListener true
             }
         }
         false
     }
+
+    private fun replaceFragment(fragment: android.support.v4.app.Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.container, fragment)
+        fragmentTransaction.commit()
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
